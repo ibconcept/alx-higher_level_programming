98330b3672f6b4942b53f7d628dd88f479ce11ff
william maina <willima09@gmail.com>
2024-01-06 11:50:43 -0800

    py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,10 @@
-#!/bin/bash
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        biggest = my_list[0]
+        for i in my_list:
+            if i > biggest:
+                biggest = i
+        return biggest
+    else:
+        return None
