eea06a850c992aa710d1e0d1c46d79bc635a3934
william maina <willima09@gmail.com>
2024-01-10 15:35:55 -0800

    b

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
-quare_matrix_simple(matrix=[]):
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
     """
     wordA function that computes the square
     value of all integers of a matrix.
