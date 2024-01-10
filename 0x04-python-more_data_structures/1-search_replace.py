2c0d56b8eb88763cc401ad97342f005879e95244
william maina <willima09@gmail.com>
2024-01-10 14:56:23 -0800

    b

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,4 @@
-#!/bin/bash
+earch_replace(my_list, search, replace):
+        if my_list is not None:
+                    return([x if x != search else replace for x in my_list])
+                    return None
