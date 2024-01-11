d77d2b204e22fbb9a725371ba5436dd59a9a1d0d
william maina <willima09@gmail.com>
2024-01-10 16:31:40 -0800

    b

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
-
-def search_replace(my_list, search, replace):
-    """
-    A function that replaces all occurrences
-    of an element by another in a new list
-    """
-    new_list = []
-    for element in my_list:
-        if element == search:
-            new_list.append(replace)
-        else:
-            new_list.append(element)
-    return new_list
+def uniq_add(my_list=[]):
+    number = 0
+    for element in set(my_list):
+        number += element
+    return numberusr/bin/python3
