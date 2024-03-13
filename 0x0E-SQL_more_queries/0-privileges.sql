03f51bbf9b0dc06b42714e378b36aff1c503448c
william maina <willima09@gmail.com>
2024-03-13 14:53:34 -0700

    b

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- Script that lists all privileges of MySQL users
+-- The Users are user_0d_1 and user_0d_2
+-- Server(in localhost)
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
